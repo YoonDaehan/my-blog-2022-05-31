ba5303c7174522aa9a7335cbd96ff8a3535b0dfe
YoonDaehan <dbseozhd1@naver.com>
2022-06-01 11:22:55 +0900

    정적이미지 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,5 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  plugins: [],
-};
-module.exports = {
-  siteMetadata: {
     title: "My First Gatsby Site",
   },
   plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
